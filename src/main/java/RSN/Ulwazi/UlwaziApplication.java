3dec8a0cd3ee4932ca74c0ecf30fb993817a24d6
RedSonAzriel <redseanational@gmail.com>
2022-01-05 16:38:01 -0500

    Wednesday progress
    Downloaded dependcies from Spring.io, created four hmtl pages, 1 css folder + sytle.css, 1 js folder + app.js + 1 svg folder + africa svg template.

---
+++ src/main/java/RSN/Ulwazi/UlwaziApplication.java
@@ -0,0 +1,13 @@
+package RSN.Ulwazi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UlwaziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UlwaziApplication.class, args);
+	}
+
+}
---
+++ src/test/java/RSN/Ulwazi/UlwaziApplicationTests.java
@@ -0,0 +1,13 @@
+package RSN.Ulwazi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UlwaziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
