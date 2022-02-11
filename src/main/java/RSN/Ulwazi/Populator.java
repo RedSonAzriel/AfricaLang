14ac9ccead2da4ea1dbfda3d103695565a8ae013
RedSonAzriel <redseanational@gmail.com>
2022-02-11 11:05:57 -0500

    Update Populator.java

--- src/main/java/RSN/Ulwazi/Populator.java
+++ src/main/java/RSN/Ulwazi/Populator.java
@@ -1,7 +1,22 @@
 package RSN.Ulwazi;
 
+import RSN.Ulwazi.Repositories.NationRepository;
+
 @Component
 public class Populator implements CommandLineRunner {
 
     private NationRepository nationRepo;
+    private CityRepository cityRepo;
+
+    public Populator(NationRepository nationRepo, CityRepository cityRepo) {
+        this.nationRepo = nationRepo;
+        this.cityRepo = cityRepo;
+    }
+
+    @Override
+    public void run(String...args) throws Exception {
+
+        //Nation List
+        
+    }
 }
